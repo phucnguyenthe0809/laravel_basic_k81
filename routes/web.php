0cd82410450823c137f475c183f725029538a037
PhucNguyenThe0809 <phucnguyenthe0809@gmail.com>
2019-10-09 19:13:20 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,131 @@ Route::group(['prefix' => 'product'], function () {
     Route::get('detail','Frontend\ProductController@getDetail' );
 });
 
+//
+Route::group(['prefix' => 'query'], function () {
+
+    Route::get('insert', function () {
+        //tham số trong insert là 1 mảng
+        //trong users có trường email,password,full,address,phone,level
+        //khởi tạo 1 bản ghi trong user
+        // DB::table('users')->insert([
+        //     'email'=>'A@gmail.com',
+        //     'password'=>'123456',
+        //     'full'=>'nguyen A',
+        //     'address'=>'khu A',
+        //     'phone'=>'1111',
+        //     'level'=>1 ]);
+
+        //tạo nhiều bản ghi cùng lúc
+        DB::table('users')->insert([
+            ['email'=>'B@gmail.com','password'=>'123456','full'=>'nguyen B','address'=>'khu B','phone'=>'2222','level'=>1 ],
+            ['email'=>'C@gmail.com','password'=>'123456','full'=>'nguyen C','address'=>'khu C','phone'=>'3333','level'=>1 ],
+            ['email'=>'D@gmail.com','password'=>'123456','full'=>'nguyen D','address'=>'khu D','phone'=>'4444','level'=>1 ]
+            ]);
+    });
+
+    //sửa sữa liệu
+
+    Route::get('update', function () {
+        //where('trường cần kiểm tra','điều kiện','giá trị so sánh');
+        //where có 2 tham số thì mặc định là =
+        DB::table('users')->where('id',9)->update(['level'=>1]);
+    });
+
+    //xoá 
+    Route::get('delete', function () {
+        //xoá theo điều kiện
+        // DB::table('users')->where('id',9)->delete();
+
+        //xoá không điều kiện (xoá tất cả bản ghi)
+        DB::table('users')->delete();
+    });
+
+    //nâng cao
+    //lấy dữ liệu bằng querybuilder
+    // chú ý : muốn lấy dữ liệu bắt buộc phải kết thúc bằng get() hoặc first
+    //lấy toàn bộ dữ liệu
+    Route::get('get-all-data', function () {
+        $users=DB::table('users')->get();
+        dd($users);
+    });
+
+
+    //lấy bản ghi đầu tiên
+    Route::get('get-first-data', function () {
+        $user=DB::table('users')->first();
+        dd($user->email);
+    });
+
+
+    //select chọn các trường cần lấy
+    Route::get('select', function () {
+        $users=DB::table('users')->select('id','full')->get();
+        dd($users);
+    });
+
+    //where () lấy dữ liệu theo điều kiện
+    Route::get('where-and', function () {
+        $users=DB::table('users')->where('id','>',13)->where('id','<',16)->get();
+        dd($users);
+    });
+
+    Route::get('where-between', function () {
+        $users=DB::table('users')->whereBetween('id',[13,15])->get();
+        dd($users);
+    });
+
+    Route::get('where-or', function () {
+        $users=DB::table('users')->where('id','<',14)->orwhere('id','>',15)->get();
+        dd($users);
+    });
+
+    //limit giới hạn bản ghi
+
+    Route::get('limit', function () {
+        //skip(1) đứng từ vị trí 1   take(2) lấy 2 phần tử
+        $users=DB::table('users')->skip(1)->take(2)->get();
+        dd($users);
+    });
+
+    //đếm số bản ghi
+
+    Route::get('count', function () {
+        $soBanGhi=DB::table('users')->count();
+        dd($soBanGhi);
+    });
+
+    //sắp xếp
+    Route::get('orderby', function () {
+        //ASC tăng dần, DESC giảm dần
+        $dlSapXep=DB::table('users')->orderby('id','DESC')->get();
+        dd($dlSapXep);
+    });
+
+    //cộng dồn
+    Route::get('sum', function () {
+        $ketQua=DB::table('users')->where('id','>',13)->sum('id');
+        dd($ketQua);
+
+    });
+
+     //cộng Tính giá trị trung bình
+     Route::get('avg', function () {
+        $ketQua=DB::table('users')->where('id','>',13)->avg('id');
+        dd($ketQua);
+
+    });
+
+
+
+});
+
+
+
+
+
+
+
 
 
 
